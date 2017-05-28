6f3b3de4cc5eb992e5f397667faa71deb47a09a1
Ming <kakacuna@gmail.com>
2017-05-28 16:09:44 -0700

    knex

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,7 @@
 const config = require('config');
 
 if (process.env.DATABASE_URL) {
-  config.knex.connection.database = process.env.DATABASE_URL;
-  config.knex.connection.user = 'viyjyvxbknaejf';
-  config.knex.connection.password = 'd5c7ad679dbb518fc5ecf364b3e9a73cb9a55c2f1b351ed164c3052ec36d4e94';
-  config.knex.connection.host = 'ec2-23-23-223-2.compute-1.amazonaws.com'
+  config.knex.connection = process.env.DATABASE_URL;
 }
 
 console.log(config.knex.connection);
