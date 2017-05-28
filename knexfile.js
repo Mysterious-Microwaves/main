e17732d2ab7a050acc1f8108e33dbc13925c8387
Ming <kakacuna@gmail.com>
2017-05-28 15:59:40 -0700

    Updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,9 @@
 const config = require('config');
 
+if (process.env.DATABASE_URL) {
+  config.knex.database = process.env.DATABASE_URL;
+}
+
+console.log(config);
+
 module.exports = config['knex'];
